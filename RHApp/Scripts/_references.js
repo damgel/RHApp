438394767a06a767a4e7b9f0e49e34d4587fe8b3
damgel <damgel@outlook.com>
2015-11-27 16:06:55 -0600

    scripts updates

--- RHApp/Scripts/_references.js
+++ RHApp/Scripts/_references.js
@@ -1,3 +1,5 @@
-﻿/// <reference path="jquery-1.9.1.js" />
-/// <reference path="jquery-ui-1.8.24.js" />
+﻿/// <reference path="jquery-ui-1.11.4.js" />
+/// <reference path="jquery-2.1.4.js" />
 /// <reference path="modernizr-2.6.2.js" />
+/// <reference path="bootstrap.min.js"  />
+/// <reference path="custom.js"  />
